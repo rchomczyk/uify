5ba93890a0ad5d73c7ea3bf18360a6facf75a91a
rafal <63915083+rchomczyk@users.noreply.github.com>
2025-01-23 11:46:41 +0100

    feat: Add time-based element transformation

--- uify-common/src/dev/shiza/uify/canvas/element/CanvasBaseElement.java
+++ uify-common/src/dev/shiza/uify/canvas/element/CanvasBaseElement.java
@@ -2,15 +2,19 @@ package dev.shiza.uify.canvas.element;
 
 import dev.shiza.uify.canvas.element.behaviour.CanvasElementGenericBehaviour;
 import dev.shiza.uify.canvas.element.cooldown.CooldownGenericBehaviour;
+import io.papermc.paper.util.Tick;
 import java.time.Duration;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.ItemStack;
 import dev.shiza.uify.scene.Scene;
 import dev.shiza.uify.canvas.Canvas;
+import org.bukkit.plugin.Plugin;
 
 public class CanvasBaseElement implements CanvasElement {
 
@@ -23,6 +27,7 @@ public class CanvasBaseElement implements CanvasElement {
         (state, event) -> {};
     private Duration cooldown = Duration.ZERO;
     private final Set<Canvas> owners = new HashSet<>();
+    private final AtomicBoolean transforming = new AtomicBoolean(false);
 
     public CanvasBaseElement(final Supplier<ItemStack> itemStack) {
         this.itemStack = itemStack;
@@ -45,6 +50,12 @@ public class CanvasBaseElement implements CanvasElement {
             ((CanvasBaseElement) element).elementDragBehaviour,
             ((CanvasBaseElement) element).elementClickBehaviour,
             ((CanvasBaseElement) element).elementCooldownBehaviour);
+    }
+
+    @Override
+    public CanvasElement cooldown(final Duration period) {
+        this.cooldown = period;
+        return this;
     }
 
     @Override
@@ -81,15 +92,35 @@ public class CanvasBaseElement implements CanvasElement {
     }
 
     @Override
-    public CanvasElement cooldown(final Duration period) {
-        this.cooldown = period;
-        return this;
-    }
-
-    @Override
     public void transform(final Supplier<ItemStack> itemStack) {
         this.itemStack = itemStack;
         this.update();
+    }
+
+    @Override
+    public void transform(final Plugin plugin, final Supplier<ItemStack> itemStack, final Duration period) {
+        transform(plugin, itemStack, this.itemStack, period);
+    }
+
+    @Override
+    public void transform(
+        final Plugin plugin, final Supplier<ItemStack> from, final Supplier<ItemStack> to, final Duration period) {
+        if (!transforming.compareAndSet(false, true)) {
+            return;
+        }
+
+        if (!Objects.equals(from, this.itemStack)) {
+            transform(from);
+        }
+
+        final long delayInTicks = Tick.tick().fromDuration(cooldown);
+        plugin.getServer()
+            .getScheduler()
+            .runTaskLater(
+                plugin, () -> {
+                    transform(to);
+                    transforming.set(false);
+                }, delayInTicks);
     }
 
     public Duration cooldown() {
--- uify-common/src/dev/shiza/uify/canvas/element/CanvasElement.java
+++ uify-common/src/dev/shiza/uify/canvas/element/CanvasElement.java
@@ -5,9 +5,12 @@ import java.util.function.Supplier;
 import org.bukkit.inventory.ItemStack;
 import dev.shiza.uify.scene.Scene;
 import dev.shiza.uify.canvas.Canvas;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.ApiStatus;
 
 public interface CanvasElement extends CanvasElementBehaviours {
+
+    CanvasElement cooldown(final Duration period);
 
     ItemStack renderElement(final Scene parentScene, final Canvas parentCanvas);
 
@@ -16,7 +19,10 @@ public interface CanvasElement extends CanvasElementBehaviours {
 
     void update();
 
-    CanvasElement cooldown(final Duration period);
-
     void transform(final Supplier<ItemStack> itemStack);
+
+    void transform(final Plugin plugin, final Supplier<ItemStack> itemStack, final Duration period);
+
+    void transform(
+        final Plugin plugin, final Supplier<ItemStack> from, final Supplier<ItemStack> to, final Duration period);
 }
